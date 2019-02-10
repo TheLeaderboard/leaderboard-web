ecfecb19ed5cc77758f56bb13af3929d81e6d73e
ben <bencrane5.12@gmail.com>
2019-02-10 14:09:54 -0500

    Add league creation page

---
+++ src/views/league/CreateLeague.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 md8 lg6>
+        <p>Create League form here</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
